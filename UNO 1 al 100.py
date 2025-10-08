7c58107f6df759c45d2e7edd7aaf715db161c0c5
LimonSEXO <pepetruenoelchico@gmail.com>
2025-10-08 13:23:13 -0600

    No se creo que funciona

--- UNO 1 al 100.py
+++ UNO 1 al 100.py
@@ -1,6 +1,15 @@
-#HOLA SOY LIMON NUM PARES MUL  del 1 al 100 solo pare
+# #HOLA SOY LIMON NUM PARES MUL del 1 al 100 solo pare
+# CAMBIO QUE PREGUNTE PARES O IMPARES SI QUIERE
 num = int(input("Dame un número porfa del 1 al 100 no mas no menos pls: "))
-print("Tabla del" ,num, "del 1 al 100:")
-for i in range(2, 101, 2):
+impares_si_no = input("¿Quieres múltiplos IMPARES en lugar de PARES? (S/N): ")
+if impares_si_no == 'S':
+    inicio = 1
+    etiqueta = "IMPARES"
+else:
+    inicio = 2
+    etiqueta = "PARES"
+
+print("Tabla del", num, "con múltiplos", etiqueta, "del 1 al 100:")
+for i in range(inicio, 101, 2):
     resultado = num * i
-    print(num, "x" ,i, "=", resultado)
+    print(num, "x", i, "=", resultado)
