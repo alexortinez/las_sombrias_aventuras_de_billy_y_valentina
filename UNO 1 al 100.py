d020fc9a09ece90b835286c89ff9ad954c41bb2e
bhyancf13 <brayannain@gmail.com>
2025-10-08 12:58:20 -0600

    Eliminar UNO 1 al 100.py

--- UNO 1 al 100.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Dame un número porfa del 1 al 100 no mas no menos pls: "))
-print("Tabla del" ,num, "del 1 al 100:")
-for i in range(1, 101):
-    resultado = num * i
-    print(num, "x" ,i, "=", resultado)
