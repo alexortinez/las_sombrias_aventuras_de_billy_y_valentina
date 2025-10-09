e221c307beb3e81947500fd51a37da0d1cbf3093
LimonSEXO <pepetruenoelchico@gmail.com>
2025-10-09 10:06:13 -0600

    ya se guardo todo bien

--- UNO 1 al 100.py
+++
@@ -1,6 +0,0 @@
-#HOLA SOY LIMON NUM PARES MUL  del 1 al 100 solo pare
-num = int(input("Dame un número porfa del 1 al 100 no mas no menos pls: "))
-print("Tabla del" ,num, "del 1 al 100:")
-for i in range(2, 101, 2):
-    resultado = num * i
-    print(num, "x" ,i, "=", resultado)
